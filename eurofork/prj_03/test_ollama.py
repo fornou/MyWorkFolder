eab25bf2e7318a97b6200baad3c1180cd068fb21
Forno <mattiaforneron@gmail.com>
2025-11-25 15:44:37 +0100

    25-11-2025
    Pulizia del repo

--- eurofork/prj_03/test_ollama.py
+++
@@ -1,8 +0,0 @@
-import ollama
-
-client = ollama.Client()
-
-prompt = "Parlami di chi sei cosa fai e perchè sei così famoso"
-response = client.generate(model="super_mario", prompt=prompt)
-
-print(response['response'])
